c103133138fe48e03039171c4811e6aad14877ef
David Soergel <dev@davidsoergel.com>
2013-11-25 12:35:36 -0500

    fix email format constraints for text fields

--- app/forms/Constraints.scala
+++ app/forms/Constraints.scala
@@ -2,25 +2,74 @@ package forms
 
 
 import edu.umass.cs.iesl.scalacommons.NonemptyString
+import edu.umass.cs.iesl.scalacommons.StringUtils._
+import scala.collection.GenTraversable
 
 case class ConstraintNotMetException(s: String = "") extends Exception(s)
 
+// the function f returns error messages, so None indicates success.
 class FieldConstraint[-T <: NestedForm[_]](errorText: String,
-                                           args: Seq[Any],
-                                           val infoText: Option[String] = None)(f: (T => Boolean)) {
-  def apply(t: T): Unit = if (!f(t)) throw new ConstraintNotMetException(errorText)
+                                           val infoText: Option[String] = None)(f: (T => Option[Option[String]])) {
+  def apply(t: T): Unit = f(t) map {
+    errorDetail => throw new ConstraintNotMetException(errorText + errorDetail.map(": " + _))
+  }
 }
 
 
-class RequiredConstraint[T <: PrefillableNestedForm[_]] extends FieldConstraint[T]("required", Nil,
-  Some("*"))(t => t.prefill.nonEmpty)
+class RequiredConstraint[T <: PrefillableNestedForm[_]] extends FieldConstraint[T]("required",
+  Some("*"))(t => if (t.prefill.nonEmpty) None else Some(None))
 
 
-class EmailConstraint[T <: PrefillableNestedForm[NonemptyString]] extends FieldConstraint[T]("Invalid email address", Nil)(t => t.prefill.flatMap(s => EmailConstraint.emailRE.findFirstIn(s.s)).isDefined)
+class EmailConstraint[T <: PrefillableNestedForm[NonemptyString]] extends FieldConstraint[T]("Invalid email address")(t => {
+  if (t.prefill.map(s => EmailParser.parseOne(s).isDefined).getOrElse(true)) None else Some(None)
+})
 
-  
-object EmailConstraint {
-  val emailRE = """(?i)^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$""".r    //http://www.regular-expressions.info/email.html 
+
+class MultiEmailConstraint[T <: PrefillableNestedForm[NonemptyString]] extends FieldConstraint[T]("Invalid email addresses")(t => {
+  val errors = t.prefill.flatMap(s => {
+    EmailParser.parseMulti(s) match {
+      case Right(x) => None
+      case Left(x) => Some(Some(x.mkString(", ")))
+    }
+  })
+  errors
+})
+
+
+case class ParsedEmail(firstName: Option[NonemptyString], lastName: Option[NonemptyString], email: NonemptyString)
+
+object EmailParser {
+  private val emailOnlyRE = """(?i)^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$""".r //http://www.regular-expressions.info/email.html 
+
+  private val nameAndEmailRE = """(?i)^(([^@]*)\s+)?(<?([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})>?)?$""".r
+
+  private val splitName = """(.*)\s+(.*)""".r
+
+  def parseOne(s: String): Option[ParsedEmail] = {
+    val nameAndEmailRE(x,fullName, y, email) = s
+
+    // could use namejuggler; just be naive for now
+    try {
+      val splitName(firstName, lastName) = fullName
+
+      email.opt.map(e => ParsedEmail(firstName.opt, lastName.opt, e))
+    }
+    catch {
+      case e: MatchError if fullName == null || fullName.trim.isEmpty => email.opt.map(e => ParsedEmail(None, None, e))
+      case e: MatchError => email.opt.map(e => ParsedEmail(None, fullName.opt, e))
+    }
+
+  }
+
+  // like Validation but without scalaz
+  def parseMulti(s: String): Either[GenTraversable[NonemptyString], GenTraversable[ParsedEmail]] = {
+    val lines = s.split("[,;\t\n]")
+    val parsedLines = lines.flatMap(_.opt).map(s => (s, parseOne(s))).toMap
+    val (success, failure) = parsedLines.partition(_._2.isDefined)
+    if (failure.nonEmpty) Left(failure.keys)
+    else Right(success.values.map(_.get))
+  }
+
 }
 
 
@@ -29,19 +78,15 @@ object EmailConstraint {
  * Otherwise such "required" fields could still be empty because we're creating a template that won't be reified yet
  * @tparam T
  */
-class TemplateCompleteConstraint[T <: PrefillableNestedForm[_]] extends FieldConstraint[T]("complete template " +
-                                                                                           "required", Nil,
-  Some("*"))(t => t.prefill.nonEmpty)
+class TemplateCompleteConstraint[T <: PrefillableNestedForm[_]] extends FieldConstraint[T]("complete template required",
+  Some("*"))(t => if (t.prefill.nonEmpty) None else Some(None))
 
 
-case class FixedConstraint[X, -T <: NestedForm[_]](fixedValue: X) extends FieldConstraint[T]("fixed value",
-  Nil)(form => {
+case class FixedConstraint[X, -T <: NestedForm[_]](fixedValue: X) extends FieldConstraint[T]("must match fixed value")(form => {
   val actual = form.get
-  actual == Some(fixedValue)
+  if (actual == Some(fixedValue)) None else Some(Some(fixedValue.toString))
 }
 )
 
 
-
-class InfoNoConstraint[T <: PrefillableNestedForm[_]](message:String) extends FieldConstraint[T]("bogus", Nil,
-  Some(message))(t =>true)
+class InfoNoConstraint[T <: PrefillableNestedForm[_]](message: String) extends FieldConstraint[T]("bogus", Some(message))(t => None)
